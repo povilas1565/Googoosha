5094e0ad19a4d0d1bb5cdae2c7d544672d0b7b2e
DanifA19 <danifeaglepro@gmail.com>
2022-11-01 10:34:05 +0300

    start

---
+++ app/src/main/java/com/example/googoosha/screens/ProfileEditFragment.kt
@@ -0,0 +1,29 @@
+package com.example.googoosha.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.googoosha.databinding.FragmentAuthBinding
+
+class ProfileEditFragment : Fragment() {
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initFuns()
+    }
+
+    private fun initFuns() {
+
+    }
+}
